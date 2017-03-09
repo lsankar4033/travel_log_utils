1459db387c398565e3ff8526202a9dff3613112d
lsankar4033 <lsankar4033@gmail.com>
2017-03-09 18:08:51 +0530

    Add logic for getting message IDs by query string

--- batch_send.py
+++ batch_send.py
@@ -14,6 +14,9 @@ SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'batch_send_secret.json'
 APPLICATION_NAME = 'batch_send'
 
+# TODO make it possible to pass this in via CLI
+QUERY_STR = "is:sent subject:('[Lakshman's travels]')"
+
 # NOTE this code was adapted from https://developers.google.com/gmail/api/quickstart/python
 def get_credentials():
     """Gets valid user credentials from local storage if it exists. Otherwise, runs the OAuth2 flow to store
@@ -22,11 +25,11 @@ def get_credentials():
     Credentials are stored in the current working directory under the .credentials subdirectory.
     """
     current_dir = os.getcwd()
-    credential_dir = os.path.join(current_dir, '.credentials')
+    credential_dir = os.path.join(current_dir, ".credentials")
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
 
-    credential_path = os.path.join(credential_dir, 'batch_send.json')
+    credential_path = os.path.join(credential_dir, "batch_send.json")
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -34,29 +37,29 @@ def get_credentials():
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
         credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
+        print("Storing credentials to " + credential_path)
     return credentials
 
-def main():
-    """Shows basic usage of the Gmail API.
-
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
+def get_message_ids(credentials, query_str):
+    """Returns a list of the first message sent on each thread defined by the provided gmail query string.
     """
-    credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+    service = discovery.build("gmail", "v1", http=http)
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    result = service.users().messages().list(userId="me", q=query_str).execute()
+    messages = result["messages"]
 
-    if not labels:
-        print('No labels found.')
-    else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
+    # Find all unique threads
+    thread_ids = set([m["threadId"] for m in messages])
 
+    # Retrieve the first message in each thread
+    first_message_ids = []
+    for thread_id in thread_ids:
+        thread_msgs = service.users().threads().get(id=thread_id, userId="me", format="minimal").execute()
+        first_message_ids.append(thread_msgs["messages"][0]["id"])
 
-if __name__ == '__main__':
-    main()
+    return first_message_ids
+
+if __name__ == "__main__":
+    credentials = get_credentials()
+    message_ids = get_message_ids(credentials, QUERY_STR)
